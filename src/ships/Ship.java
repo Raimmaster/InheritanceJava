8a6a95dd5d61bcfa064675590a381179622ce1c4
Raimmaster <rai.interiano@gmail.com>
2015-07-14 15:13:04 -0600

    Ship class created

---
+++ src/ships/Ship.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ships;
+
+import java.util.Calendar;
+import java.util.Formatter;
+
+/**
+ *
+ * @author Raim
+ */
+public abstract class Ship {
+    protected String name;
+    protected Calendar circulationDate;
+    
+    public Ship(String nom){
+        name = nom;
+        circulationDate = Calendar.getInstance();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Calendar getCirculationDate() {
+        return circulationDate;
+    }
+
+    public void setCirculationDate(Calendar circulationDate) {
+        this.circulationDate = circulationDate;
+    }
+    
+    public abstract void agregarElemento();
+    
+    public abstract double vaciarCobrar();
+    
+    @Override
+    public String toString(){
+        Formatter formato = new Formatter();
+        formato.format("%tF", circulationDate);
+        return "Name: " + name + " - Circulation Date: " + formato.toString();
+    }
+}
